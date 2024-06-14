9df5b504640c3fbdaec7643d7849824739e6d466
Sadhanajx10 <sohmasadhana@gmai.com>
2024-06-14 00:44:45 -0500

    Initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,8 @@ import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/user.routes.js";
+
 import connectMongoDB from './db/connectMongoDB.js';
 
 
@@ -28,6 +30,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 
 app.listen(PORT, () =>{
